b0f4607f8ceb9632eac1edfd61a0c5cf624031c6
koszmarek <mkotajny@gmail.com>
2019-05-18 17:41:42 +0200

    github Issue #6 : refactoring of communication with API - by using axios and async/await

--- src/components/stateful/CvPage/CvPage.js
+++ src/components/stateful/CvPage/CvPage.js
@@ -1,45 +1,63 @@
 import React, { Component } from 'react';
 import classes from './CvPage.css';
+import axios from 'axios';
 
 class CvPage extends Component {
   constructor() {
     super();
     this.state = {
-      name: "",
-      avatarUrl: "",
-      email: "",
-      webPage: ""
+      data: {
+        name: "",
+        avatarUrl: "",
+        webPage: ""
+      },
+      isLoading: false,
+      error: null
+    }
+  }
+
+  async getData() {
+    this.setState({ isLoading: true });
+
+    try {
+      const result = await 
+        axios.get('https://api.github.com/users/' + this.props.match.params.login);
+
+      this.setState({
+        data: {
+          name: result.data.name,
+          avatarUrl: result.data.avatar_url,
+          webPage: result.data.blog
+        },
+        isLoading: false
+      });
+    } catch (error) {
+      this.setState ({
+        error: true,
+        isLoading: false
+      });
     }
   }
 
   componentDidMount() {
-    fetch('https://api.github.com/users/' + this.props.match.params.login)
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      this.setState({
-        name: data.name,
-        avatarUrl: data.avatar_url,
-        webPage: data.blog
-      });
-    })
+    this.getData();
   }
 
   render() {
-    console.log("render: " + this.state.name);        
+    console.log("render: " + this.state.data.name);        
     console.log(this.props);        
     return (
       <React.Fragment>
         <div className={classes.mainDetails}> 
           <div className={classes.headShot}>
-            <img src={this.state.avatarUrl} alt={this.state.name} />
+            <img src={this.state.data.avatarUrl} alt={this.state.data.name} />
           </div>
           <div className={classes.name}>
-            <h1>{this.state.name}</h1>
+            <h1>{this.state.data.name}</h1>
             <h2>Github Profile</h2>
           </div>
           <div className={classes.contactDetails}>
-            <a href={this.state.webPage}>{this.state.webPage}</a>
+            <a href={this.state.data.webPage}>{this.state.data.webPage}</a>
           </div>
           <div className={classes.clear}></div>
         </div>
