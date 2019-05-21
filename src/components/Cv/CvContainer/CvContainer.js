6ca8f6cc3d8e793f827a1f94fa9e66c6b722e0a6
koszmarek <mkotajny@gmail.com>
2019-05-21 11:40:42 +0200

    github Issue #10 : add "home" button in the cv page
    The button redirects to the landing page.

--- src/components/Cv/CvContainer/CvContainer.js
+++ src/components/Cv/CvContainer/CvContainer.js
@@ -4,7 +4,7 @@ import CvDataCollector, {} from '../CvDataCollector/CvDataCollector';
 import CvHeadlineSection from '../CvSectionInstances/CvHeadlineSection/CvHeadlineSection';
 import CvLanguagesSection from '../CvSectionInstances/CvLanguagesSection/CvLanguagesSection';
 import CvSectionContent from '../CvSectionTemplates/CvSectionContent/CvSectionContent';
-
+import {IoMdHome} from "react-icons/io";
 
 class CvContainer extends Component {
 
@@ -12,9 +12,8 @@ class CvContainer extends Component {
     super();
     this.state = {
       data: null,
-      error: false
+      error: false }
     }
-  }
 
   handleCvData = (response) => {
     if (response.error) {
@@ -26,28 +25,33 @@ class CvContainer extends Component {
 
   render() {
     if (!this.state.data) {
-        return (
-          <CvDataCollector OnResponse={this.handleCvData} 
-            login={this.props.match.params.login}/>
-        )
+      return (
+        <CvDataCollector OnResponse={this.handleCvData} 
+          login={this.props.match.params.login}/>)
     } else {
       return (
-        <div className={classes.CvContainer}>            
-          <CvHeadlineSection name={this.state.data.user.name}
-            avatarUrl={this.state.data.user.avatar_url}
-            webPageUrl={this.state.data.user.blog}/>
+        <React.Fragment>
+          <IoMdHome className={classes.home} 
+            onClick={() => this.props.history.push('/')}/>
           
-          <div className={classes.mainArea}>
-            <CvSectionContent title="Overview">
-              Chris Wanstrath is a developer with <strong>{this.state.data.user.public_repos}</strong> public repositories and <strong>{this.state.data.user.followers}</strong> followers.
-            </CvSectionContent>
-            <CvLanguagesSection repositories={this.state.data.repositories}/>
-            <CvSectionContent title="About this resume">
-              This résumé is generated automatically using public information from the developer's GitHub account. Do not hesitate to visit <a href={"https://github.com/"+this.state.data.user.login}>{this.state.data.user.name}'s GitHub page</a> for a complete work history.
-            </CvSectionContent>
+          <div className={classes.CvContainer}>            
+            <CvHeadlineSection name={this.state.data.user.name}
+              avatarUrl={this.state.data.user.avatar_url}
+              webPageUrl={this.state.data.user.blog}/>
+            
+            <div className={classes.mainArea}>
+              <CvSectionContent title="Overview">
+                Chris Wanstrath is a developer with <strong>{this.state.data.user.public_repos}</strong> public repositories and <strong>{this.state.data.user.followers}</strong> followers.
+              </CvSectionContent>
+
+              <CvLanguagesSection repositories={this.state.data.repositories}/>
+
+              <CvSectionContent title="About this resume">
+                This résumé is generated automatically using public information from the developer's GitHub account. Do not hesitate to visit <a href={"https://github.com/"+this.state.data.user.login}>{this.state.data.user.name}'s GitHub page</a> for a complete work history.
+              </CvSectionContent>
+            </div>
           </div>
-        </div>
-      )
+        </React.Fragment>)
     }
   }
 }
